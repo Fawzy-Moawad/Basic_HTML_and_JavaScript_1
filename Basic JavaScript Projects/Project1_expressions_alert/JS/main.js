f67a344ea4ec6a0f35cab4c00e2bdf850cc9ca77
Fawzy Moawad <fawzy3arafa88@gmail.com>
2023-06-02 17:05:46 -0700

    add two functions and var

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+alert ("Hi, I'm Fawzy");            //this to make alert pop up on the screen.
+window.alert ("Subscribe Me");      //this to make alert pop up on the screen.
+var X = "New JS Line"       //this to creat a var and print it.
+document.write(X);
+document.write(3 + 3);      //this to multiply.  
+var B = "concatenated"+"string"     //this to concatenated string and print it.
+document.write(B);
+var Family = "Moawad", Dad = "Arafa", Mom = "Doha", Daughter = "Fatma", Son = ("Mohamed" + "Ahmad" + "Fawzy");//this to creat a var and choose what to print from it.
+document.write(Dad);
+
+function myFunction() {         //this to concatenated strings and print it after you do the action wrote in html file.
+    var sentence = "I am Learning";
+    sentence += "a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+function My__First_Function() {     //this function to make a button.
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
