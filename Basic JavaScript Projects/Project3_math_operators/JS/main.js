420fb1f37af151d7275cd016b0d34dab73f82ca7
Fawzy Moawad <fawzy3arafa88@gmail.com>
2023-07-10 20:55:41 -0700

    dd functions to the HTML file

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,4 @@
 function addition_function(Math) {
-    var additionFunction = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 =" + additionFunction;
+    let additionFunction = 2 + 2;
+    document.getElementById(Math).innerHTML = "2 + 2 =" + additionFunction;
 }
