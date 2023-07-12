3b4ce02d28d4e2b33f18d9cf15d17c1e9e794aeb
Fawzy Moawad <fawzy3arafa88@gmail.com>
2023-07-11 17:20:42 -0700

    add rest of the functions

--- Basic JavaScript Projects/Project_9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project_9_countdown_slideshow/JS/main.js
@@ -1,3 +1,4 @@
+//countdown timer function
 function countdown() {
     let seconds = document.getElementById("seconds").ariaValueMax;
 
@@ -11,4 +12,36 @@ function countdown() {
         }
     }
     tick();
+}
+//imag slideshow functions
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//next/previous controls
+function plusSlide(n) {
+    showSlides(slideIndex+= n);
+}
+//thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[1].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "active";
 }
