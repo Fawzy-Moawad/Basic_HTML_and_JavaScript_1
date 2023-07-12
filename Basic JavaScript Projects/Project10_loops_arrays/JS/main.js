da853f520976d3f71382b44d02d9cba048474e11
Fawzy Moawad <fawzy3arafa88@gmail.com>
2023-07-11 18:26:22 -0700

    creeat functions

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,51 @@
+//A function using a while loop
+function count_to_Ten() {
+    let Digit = "";
+    let x = 1;
+    while (x < 11) {
+        Digit += "<br>" + x;
+        x++;
+    }
+document.getElementById("counring_to_Ten").innerHTML = Digit;
+}
+
+//A function using a for loop
+let Instruments = [
+    "Guitar",
+    "Drums",
+    "Piano",
+    "Bass",
+    "Violin",
+    "Trumpet",
+    "Flute",
+];
+let content = "";
+let y;
+function for_Loop() {
+    for (y=0; y < Instruments.length; y++) {
+        content +[ Instruments[y]] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = content;
+}
+
+//A function with an array
+function color_array() {
+    let colors = [];
+    colors[1] = "Green";
+    colors[2] = "Red";
+    colors[3] = "Blue";
+    colors[4] = "Yellow";
+    colors[5] = "Orange";
+    colors[6] = "Purple";
+    document.getElementById("Array").innerHTML = "My favourite color is " + colors[6] + ".";
+}
+
+//creating an object with the let keyword
+function car() {
+    let car = {
+        make: "Nissan",
+        year: "2023",
+        color: "Black",
+    };
+    document.getElementById("car").innerHTML = "I have a " + car.year + " " + car.color + " " + car.make + ".";
+}
